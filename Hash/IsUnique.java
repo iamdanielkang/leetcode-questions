916a65daf09ba979636b3549de3f309ea149ba70
Daniel <danielkang252@gmail.com>
2020-12-18 14:42:18 +1100

    Added a second way to solve IsUnique by simply traversing through values after current character. If they end up equalling, then clearly it must be false

--- Hash/IsUnique.java
+++ Hash/IsUnique.java
@@ -11,15 +11,32 @@ public class IsUnique {
         String str = "123456789a0";
 		
 		System.out.println("String: " + str);
-		System.out.println("Hashmap: " + isUnique(str));
+		System.out.println("Hashmap: " + isUnique2(str));
 		
 		System.out.println();
 		
 		str = "AbCcdefbz";
 		System.out.println("String: " + str);
-		System.out.println("Hashmap: " + isUnique(str));
+		System.out.println("Hashmap: " + isUnique2(str));
     }
 
+	// Second method using an array. Essentially, I check that the values after the current char are not 
+	// the same, otherwise, you return false.
+	public static Boolean isUnique2(String s) {
+
+		for (int i = 0; i < s.length(); i++){
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j))
+                    return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	// This method checks to see if the value at the given key is null
+	// if it isn't, then there is a duplicate, therefore false.
     public static Boolean isUnique(String s) {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
 		
